cecad396fd0486741f814ff08b3cec5d51dea61e
Poojita Raj <poojiraj@amazon.com>
2022-05-25 13:14:52 -0700

    implementing segment rep target
    Signed-off-by: Poojita Raj <poojiraj@amazon.com>

--- server/src/main/java/org/opensearch/indices/replication/SegmentReplicationState.java
+++ server/src/main/java/org/opensearch/indices/replication/SegmentReplicationState.java
@@ -27,7 +27,9 @@ public class SegmentReplicationState implements ReplicationState {
     public enum Stage {
         INIT((byte) 0),
 
-        DONE((byte) 1);
+        REPLICATING((byte) 1),
+
+        DONE((byte) 2);
 
         private static final Stage[] STAGES = new Stage[Stage.values().length];
 
@@ -56,29 +58,58 @@ public class SegmentReplicationState implements ReplicationState {
         }
     }
 
-    public SegmentReplicationState() {
-        this.stage = Stage.INIT;
-    }
-
     private Stage stage;
+    private final ReplicationLuceneIndex index;
+    private final ReplicationTimer timer;
+
+    public SegmentReplicationState(ReplicationLuceneIndex index) {
+        stage = Stage.DONE;
+        this.index = index;
+        timer = new ReplicationTimer();
+        timer.start();
+    }
 
     @Override
     public ReplicationLuceneIndex getIndex() {
-        // TODO
-        return null;
+        return index;
     }
 
     @Override
     public ReplicationTimer getTimer() {
-        // TODO
-        return null;
+        return timer;
     }
 
     public Stage getStage() {
         return stage;
     }
 
+    protected void validateAndSetStage(Stage expected, Stage next) {
+        if (stage != expected) {
+            assert false : "can't move recovery to stage [" + next + "]. current stage: [" + stage + "] (expected [" + expected + "])";
+            throw new IllegalStateException(
+                "can't move recovery to stage [" + next + "]. current stage: [" + stage + "] (expected [" + expected + "])"
+            );
+        }
+        stage = next;
+    }
+
     public void setStage(Stage stage) {
-        this.stage = stage;
+        switch (stage) {
+            case INIT:
+                this.stage = Stage.INIT;
+                getIndex().reset();
+                break;
+            case REPLICATING:
+                validateAndSetStage(Stage.INIT, stage);
+                getIndex().start();
+                break;
+            case DONE:
+                validateAndSetStage(Stage.REPLICATING, stage);
+                getIndex().stop();
+                getTimer().stop();
+                break;
+            default:
+                throw new IllegalArgumentException("unknown SegmentReplicationState.Stage [" + stage + "]");
+        }
     }
 }
