b740e82f5c3eb0b43baf2b3c2ab50633db40522e
Poojita Raj <poojiraj@amazon.com>
2023-06-07 15:53:29 -0700

    update map
    Signed-off-by: Poojita Raj <poojiraj@amazon.com>

--- server/src/main/java/org/opensearch/index/codec/CodecService.java
+++ server/src/main/java/org/opensearch/index/codec/CodecService.java
@@ -83,6 +83,7 @@ public class CodecService {
 
     public void loadMap() {
         versionStringMap.put(Version.fromString("3.0.0"), "Lucene95");
+        versionStringMap.put(Version.fromString("2.9.0"), "Lucene95");
         versionStringMap.put(Version.fromString("2.8.0"), "Lucene95");
         versionStringMap.put(Version.fromString("2.7.1"), "Lucene95");
         versionStringMap.put(Version.fromString("2.7.0"), "Lucene95");
